e7db222f467ea0433e691c1846bc048183c36fad
bagleee <Bagl.Coop@gmail.com>
2025-05-31 13:09:01 +0300

    login page view

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 
 def main():
-    st.title("Hello World from Streamlit!")
+    
+    auth_type = st.sidebar.radio("Choose the action", ["Login", "Register"])
+    
+    if auth_type == "Login":
+        with st.form("login_form"):
+            username = st.text_input("User name")
+            password = st.text_input("Password", type="password")
+            submit = st.form_submit_button("Login")
+            
+            if submit:
+                print('LogIn',username, password)
+    else:
+        with st.form("register_form"):
+            username = st.text_input("User name")
+            password = st.text_input("Password", type="password")
+            submit = st.form_submit_button("Create account")
+            
+            if submit:
+                print('Register',username, password)
 
 if __name__ == '__main__':
     main()
